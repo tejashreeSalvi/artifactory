434f9d04cecb8d5e67fbc56a89c3831d1f749641
sumitchansoriya <69456228+sumitchansoriya@users.noreply.github.com>
2022-08-24 01:59:03 +0530

    Update dependencyIdentifier.py

--- dependencyIdentifier.py
+++ dependencyIdentifier.py
@@ -2,19 +2,14 @@ import xml.etree.ElementTree as xml
 import glob
 
 path = "../../../.m2/repository"
-
+m2_dependencies = ""
 text_files = glob.glob(path + "/**/*.jar", recursive = True)
-file1 = open("m2_dependencies.txt","w")
-  
-file1.writelines(text_files)
-print("All jar in m2_dependencies.txt file")
-file1.close()
-#    find ./ -name "*.jar" >> test.txt
+for element in text_files: 
+        m2_dependencies += element+";"
+#find ./ -name "*.jar" >> test.txt
 print("---------------------------------------------------")
-file2 = open("m2_dependencies.txt","r")
-file3 = open("missing_dependencies.txt","w")
-readfile = file2.read()
-#print(readfile,"******************")
+file1 = open("missing_dependencies.txt","w")
+file2 = open("dependencies_compID.txt","w")
 pom = xml.parse('pom.xml')
 
 nsmap = {'m': 'http://maven.apache.org/POM/4.0.0'}
@@ -25,14 +20,16 @@ for mapping in pom.findall('//m:dependencies/m:dependency', nsmap):
     
     dependency_name=artifactId+"-"+version+".jar"
     dependency_path=groupId.replace(".","/")+"/"+artifactId+"/"+version+"/"+dependency_name
+    dependency_compID="gav://"+groupId+":"+artifactId+":"+version
     # checking condition for string found or not
-    if dependency_name in readfile: 
+    if dependency_name in m2_dependencies: 
         print(dependency_name, 'Found In File')
     else: 
         print(dependency_name , 'Not Found')
-        file3.write(dependency_path+";") 
+        file1.write(dependency_path+";") 
+        file2.write(dependency_compID+"\n") 
   
 # closing a file
+file1.close()
 file2.close()
-file3.close()
 
